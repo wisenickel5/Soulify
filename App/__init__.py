da5dadae9171e4ec68ef3d7683a91523a3d5c2dc
Dylan Alexander <dalexander@carterandclark.com>
2021-11-21 09:01:17 -0600

    Continuing to debug import errors

--- App/__init__.py
+++ App/__init__.py
@@ -4,32 +4,29 @@ from flask_bootstrap import Bootstrap
 from apscheduler.schedulers.background import BackgroundScheduler
 import sqlalchemy
 
-def create_app() -> Flask:
-    app = Flask(__name__)
-    app.config.from_object(config)
-    app.secret_key = app.config['CLIENT_SECRET']
+app = Flask(__name__)
+app.config.from_object(config)
+app.secret_key = app.config['CLIENT_SECRET']
 
-    # connect to local instance of mysqlDB server
-    engine = sqlalchemy.create_engine('sqlite:///:memory:', echo=True)
+# connect to local instance of mysqlDB server
+engine = sqlalchemy.create_engine('sqlite:///:memory:', echo=True)
 
-    # create session and base declarative
-    from sqlalchemy.orm import sessionmaker
-    Session = sessionmaker(bind=engine)
+# create session and base declarative
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
 
-    from sqlalchemy.ext.declarative import declarative_base
-    Base = declarative_base()
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
-    # make sure user table is created
-    from App.user_operations import User
-    Base.metadata.create_all(engine)
+# make sure user table is created
+from App.user_operations import User
+Base.metadata.create_all(engine)
 
-    # schedule updates for the TopTracks playlists
-    from App.services import updatePlaylists
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(updatePlaylists, trigger='interval', days=1)
-    scheduler.start()
+# schedule updates for the TopTracks playlists
+from App.services import updatePlaylists
+scheduler = BackgroundScheduler()
+scheduler.add_job(updatePlaylists, trigger='interval', days=1)
+scheduler.start()
 
-    import App.routes
-    bootstrap = Bootstrap(app)
-    
-    return app
+import App.routes
+bootstrap = Bootstrap(app)
--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-from App import create_app
-
-app = create_app()
+from App import app
 
 if __name__ == "__main__":
     app.run(debug=True)
